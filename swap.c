9c3fcf1d1abb5895e3cd3d76f19aad1a9ad582f8
SreeHari77 <84658632+SreeHari77@users.noreply.github.com>
2021-06-30 11:26:57 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -3,21 +3,25 @@
 
 void swap_max(int arr[],int l,int n){
 int max=arr[n];
-int pos=n;
-for(int i=n;i<l;i++){
+int pos=-1;
+for(int i=n+1;i<l;i++){
 if(arr[i]>max){
 max=arr[i];
 pos=i;
 }
 }
+ if(pos!=-1){
 arr[pos]=arr[n];
 arr[n]=max;
- 
+ }
+ return 0;
 }
 
-int ssort(int arr[],int l){
-  for(int i=0;i<l;i++){
-    swap_max(arr[],l,i);
+void ssort(int arr[],int l){
+  
+ for(int i=0;i<l;i++){
+   swap_max(arr[],l,i);
   }
+ return 0;
   
 }
